23340a18de945c519d7daddb9939cf2adda45ef9
Alexander Sh <shilinsasha16@gmail.com>
2021-11-23 23:20:00 +0300

    Add SPI files

---
+++ Src/SPI.c
@@ -0,0 +1,29 @@
+
+#include "stm32f1xx.h"
+#include "SPI.h"
+
+// Function of initialization SPI1 for connect to BMP280
+
+void SPI1_Init(void)
+{
+	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;				    // Enable clock of SPI1.
+	SPI1->CR1 = 0;
+	SPI1->CR2 = 0;
+	SPI1->CR1 |= SPI_CR1_BR_1;     								// Clock is divide by 8.
+	SPI1->CR1 |= SPI_CR1_SSM | SPI_CR1_SSI;			  // Ignore NSS pin IO value.
+	
+	SPI1->CR1 |= SPI_CR1_MSTR | SPI_CR1_SPE;			// Master mode, SPI enable.
+}
+
+// Function of initialization SPI2 for imitation protocol of WS2812b
+
+void SPI2_Init(void)
+{
+	RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;				    // Enable clock of SPI2.
+	SPI2->CR1 = 0;
+	SPI2->CR2 = 0;
+	SPI2->CR1 |= SPI_CR1_BR_1;     								// Clock is divide by 8.
+	SPI2->CR1 |= SPI_CR1_SSM | SPI_CR1_SSI;			  // Ignore NSS pin IO value.
+	
+	SPI2->CR1 |= SPI_CR1_MSTR | SPI_CR1_SPE;			// Master mode, SPI enable.		
+}
